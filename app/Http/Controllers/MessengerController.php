609188828a6688387df29b23440603fec4491506
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2024-08-08 02:13:21 +0300

    no results to show when the query returns null

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -16,11 +16,14 @@ class MessengerController extends Controller
         $users = User::whereLike('name', '%'.$query.'%')
                 ->orWhereLike('username', '%'.$query.'%')
                 ->paginate(10);
-
+        
         $html = '';
         foreach ($users as $user) {
             $html .= view('messenger.components.search-user-component', ['user' => $user])->render();
         }
+
+        if(!$html)
+            $html = '<p class="text-center">No Results To Show</p>';
 
         return response()->json(['html' => $html, 'last_page' => $users->lastPage()]);
     }
